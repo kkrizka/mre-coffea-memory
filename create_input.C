55f3ff45bdf59883d5a11458be7e6da388471d71
Karol Krizka <kkrizka@gmail.com>
2022-07-15 22:11:55 -0700

    Use TTree with a /g branch.

---
+++ create_input.C
@@ -0,0 +1,20 @@
+#include <TFile.h>
+#include <TTree.h>
+
+void create_input()
+{
+    TFile *fh=TFile::Open("input.root", "RECREATE");
+
+    TTree *t = new TTree("Events", "");
+
+    unsigned long br_unsignedlong = 0;
+    t->Branch("br_unsignedlong", &br_unsignedlong);
+
+    for(uint32_t i=0; i<=0xFFFFFFF; i++) {
+        br_unsignedlong = i;
+        t->Fill();
+    }
+    t->Write();
+
+    fh->Close();
+}
